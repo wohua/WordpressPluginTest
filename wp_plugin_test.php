0751b32a5a5248738cf39ef4fd0887aea393eb55
Lu Sun <sunlufinal@hotmail.com>
2015-07-14 16:19:00 -0400

    Show Google Map

--- wp_plugin_test.php
+++ wp_plugin_test.php
@@ -12,14 +12,31 @@ Domain Path: /languages
 Text Domain: my-toolset
 */
 
-function plugin_test_shortcode()
+function display_map_shortcode()
 {
-	return "plugin test";
+	return "<style type=\"text/css\">
+      html, body, #map-canvas { width: 100%; height: 500px; margin: 0; padding: 0;}
+    </style>
+    <script type=\"text/javascript\"
+      src=\"https://maps.googleapis.com/maps/api/js?key=AIzaSyCcp4mJQHbpnxU-pfWNoE4AWGYvX2Wv2qU\">
+    </script>
+    <script type=\"text/javascript\">
+      function initialize() {
+        var mapOptions = {
+          center: { lat: -34.397, lng: 150.644},
+          zoom: 8
+        };
+        var map = new google.maps.Map(document.getElementById('map-canvas'),
+            mapOptions);
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
+    </script>
+    <div id=\"map-canvas\"></div>";
 }
 
-function plugin_test_register_shortcode()
+function display_map_register_shortcode()
 {
-	add_shortcode('plugin_test', 'plugin_test_shortcode');
+	add_shortcode('plugin_test_display_map', 'display_map_shortcode');
 }
 
-add_action('init', 'plugin_test_register_shortcode');
+add_action('init', 'display_map_register_shortcode');
