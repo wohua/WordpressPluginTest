a8c8adc535dc1ba9b567794d45a483b0391955c7
Lu Sun <sunlufinal@hotmail.com>
2015-07-14 14:08:40 -0400

    File Header

---
+++ wp_plugin_test.php
@@ -0,0 +1,13 @@
+<?
+/*
+Plugin Name: Wordpress Plugin Test
+Plugin URI:  http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: This describes my plugin in a short sentence
+Version:     0.0
+Author:      Fallland Lu
+Author URI:  http://URI_Of_The_Plugin_Author
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Domain Path: /languages
+Text Domain: my-toolset
+*/
